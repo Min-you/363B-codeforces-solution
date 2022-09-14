33124b3a2cea2ee35f0ed40e268eacb428e5b088
Min-you <101444459+Min-you@users.noreply.github.com>
2022-09-14 20:08:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-int main(){
-    int n, k;
+int main(){ 
+    ll n, k, x, ans = 0;
     cin >> n >> k;
-    vector <int> height;
-    for (int i = 0; i < n; i++){
-        int temp;
-        cin >> temp;
-        height.push_back(temp);
+    ll ara[n], sum[n];
+    for (int i = 0; i < n; i++) {
+        cin >> ara[i];
+        if (i == 0) sum[0] = ara[0];
+        else sum[i] = sum[i-1] + ara[i];
     }
-    sort(height.begin(), height.end());
-    int sum = 0;
-    for (int i = 0; i < k; i++){
-        sum += height[i];
+
+    ll mn = (ll) 1e16;
+    for (int i = 0; i < n-k+1; i++) {
+        if (i == 0) x = sum[i+k-1];
+        else x = sum[i+k-1] - sum[i-1];
+        if (x < 0) x *= -1;
+        if (x < mn) {
+            mn = x;
+            ans = (ll) i;
+        }
     }
-    cout << sum << endl;
+    cout << ans+1 << endl;
+    return 0;
 }
